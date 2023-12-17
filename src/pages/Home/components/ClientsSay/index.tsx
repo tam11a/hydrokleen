c92a214d6dd7dde4f5e4897c37bae1063e878743
tam11a <ibrahimsadiktamim@gmail.com>
2023-12-18 02:53:57 +0600

    client say responsive

--- src/pages/Home/components/ClientsSay/index.tsx
+++ src/pages/Home/components/ClientsSay/index.tsx
@@ -38,15 +38,21 @@ const ClientsSay: React.FC = () => {
 	return (
 		<div className="grid grid-cols-7 gap-2">
 			<div className="col-span-7 md:col-span-4">
-				<h2 className="text-3xl font-bold text-end mt-4">
+				<h2 className="text-3xl font-bold text-center md:text-end mt-4">
 					What Our
 					<br />
 					<span className="text-primary-600">Clients Say</span>
 				</h2>
-				<p className="text-sm font-semibold text-slate-600 text-end mb-5 mt-3">
+				<p className="text-sm font-semibold text-slate-600 text-center md:text-end mb-5 mt-3">
 					Valuable Feedbacks from Our Clients
 				</p>
 				<Swiper
+					centeredSlides={true}
+					autoplay={{
+						delay: 3500,
+					}}
+					loop={true}
+					spaceBetween={30}
 					navigation={{
 						prevEl: navigationPrevRef.current,
 						nextEl: navigationNextRef.current,
@@ -62,16 +68,16 @@ const ClientsSay: React.FC = () => {
 				>
 					{feedbacks.map((feedback, index) => (
 						<SwiperSlide key={index}>
-							<div className="flex flex-col items-end p-3 justify-center">
-								<p className="text-sm font-semibold text-right text-slate-600 max-w-lg">
+							<div className="flex flex-col items-center md:items-end p-3 justify-center">
+								<p className="text-sm font-semibold text-center md:text-right text-slate-600 max-w-lg">
 									" {feedback.feedback} "
 								</p>
-								<div className="flex flex-row gap-2 mt-3 items-center justify-start">
+								<div className="flex flex-col-reverse md:flex-row gap-2 mt-3 items-center justify-start">
 									<div>
-										<h3 className="text-xl font-bold text-end">
+										<h3 className="text-xl font-bold text-center md:text-end">
 											{feedback.name}
 										</h3>
-										<p className="text-xs font-semibold text-end text-slate-600">
+										<p className="text-xs font-semibold text-center md:text-end text-slate-600">
 											{feedback.designation}
 										</p>
 									</div>
@@ -85,7 +91,7 @@ const ClientsSay: React.FC = () => {
 						</SwiperSlide>
 					))}
 				</Swiper>
-				<div className="flex flex-row items-center justify-end text-primary-600">
+				<div className="flex flex-row items-center justify-center md:justify-end text-primary-600">
 					<IconButton
 						color="primary"
 						id="prev-review"
