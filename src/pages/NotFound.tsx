469f3d4b0dcab4ac6bf04b4c8e80dab8763ee8b8
Ibrahim Sadik Tamim <66836027+tam11a@users.noreply.github.com>
2023-12-18 16:00:57 +0600

    added: 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const NotFound: React.FC = () => {
-	return <div>NotFound</div>;
+	return (
+		<div>
+			<img
+				src="/404.svg"
+				className="max-w-xl mx-auto"
+			/>
+		</div>
+	);
 };
 
 export default NotFound;
